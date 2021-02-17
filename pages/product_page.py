ffde71d8c8016a355b91492c024832db1f06db06
Sanovski44 <sanovski44@gmail.com>
2021-02-17 16:42:27 +0300

    Add test: adding to basket from product page

---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+
+class ProductPage(BasePage):
+
+    def add_to_basket(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON).click()
+
+    def correct_book_price(self):
+        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE_BASKET).text
+        assert book_price == basket_price, "Price is not same"
+
+    def correct_book_name(self):
+        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+        book_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_BASKET).text
+        assert book_name == book_basket, "Name is not same"
+
+    def correct_success_message(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is not find"
+
+
---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+import time
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.correct_success_message()
+    page.correct_book_price()
+    page.correct_book_name()
+
